b89d67221cec32b8c6cd4b9548ee19bd41679dc6
Rodin Grajo <rodin534@revature.net>
2022-09-21 15:54:33 -0500

    added user model

---
+++ YoinkAPI/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Models;
+public class User
+{
+    string? userID;
+    int? role;
+    DateTime? dateCreated;
+    DateTime? dateModified;
+
+    public User()
+    {
+    }
+
+    public User(string? userID, int? role, DateTime? dateCreated, DateTime? dateModified)
+    {
+        this.userID = userID;
+        this.role = role;
+        this.dateCreated = dateCreated;
+        this.dateModified = dateModified;
+    }
+}
