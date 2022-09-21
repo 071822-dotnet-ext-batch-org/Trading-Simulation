fea3ebb44615505c1b799f84d25b15724e846755
Chrisjon Perry <chrisjonperry@Chrisjons-MacBook-Pro.local>
2022-09-21 18:30:26 -0400

    adding othermodels

---
+++ YoinkAPI/Models/LikeComments.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Models
+{
+    public class LikeComments
+    {
+        public Guid likesCommentsID { get; set; }
+        public Guid fk_commentID { get; set; }
+        public string fk_userID { get; set; }
+        public DateTime dateCreated { get; set; }
+        public DateTime dateModified { get; set; }
+
+
+
+    }
+}
+
---
+++ YoinkAPI/Models/LikesPosts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Models
+{
+    public class LikesPosts
+    {
+        public Guid likesPostsID { get; set; }
+        public Guid fk_postID { get; set; }
+        public string fk_userID { get; set; }
+        public DateTime dateCreated { get; set; }
+        public DateTime dateModified { get; set; }
+        public LikesPosts()
+        {
+
+        }
+    }
+}
+
+
