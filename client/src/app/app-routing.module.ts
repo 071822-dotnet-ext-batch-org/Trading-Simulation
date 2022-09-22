1349b51d7c99adea5d62b6d8c0f34f336b8ee368
Larry Pallozzi <109549983+LarryPallozziRevature1@users.noreply.github.com>
2022-09-22 12:01:41 -0400

    nav bar buttons

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
 
 const routes: Routes = [];
 
