fdf52b0f186bc5e2a08dee9ceb98f1b64edadc9b
Mohammad Ibrahim <mohammad960@revature.net>
2022-09-23 09:37:24 -0500

    commit

--- YoinkAPI/RepoLayer/IdbsRequests.cs
+++ YoinkAPI/RepoLayer/IdbsRequests.cs
@@ -7,5 +7,9 @@ namespace RepoLayer
         Task<Profile?> CreateProfileAsync(string userID, string Name, string Email, int Privacy);
         Task<Profile?> EditProfileAsync(string userID, string Name, string Email, int Privacy);
         Task<Profile?> GetProfileByUserIDAsync(string userID);
+        Task<List<Buy?>> GetAllBuyBySymbolAsync(string value);
+        Task<bool?> AddNewSellAsync(Guid PortfolioId, string Symbol, decimal amountSold, decimal priceSold, DateTime dateSold);
+        Task<Portfolio?> CreatePortfolioAsync(string? auth0Id, Portfolio? p);
+        Task<Portfolio?> GetPortfolioByUserIDAsync(string? auth0Id);
     }
 }
