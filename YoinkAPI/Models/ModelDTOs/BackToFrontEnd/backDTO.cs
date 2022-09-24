ec5bc716ce9db9b75518f36da238c729a6d2e7ed
sendes <sdhaiti.business@gmail.com>
2022-09-24 07:32:28 -0400

    This is an update to Models Folder adding ModelDTOs folder to it. ANd Adding 2 classes of BackEndDTO's and FrontEndDTO's

---
+++ YoinkAPI/Models/ModelDTOs/BackToFrontEnd/backDTO.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.ModelDTOs.BackToFrontEnd
+{
+    /// <summary>
+    /// This is the porfolio DTO to get updates from the front end
+    /// </summary>
+    public class Portfolio{
+        public Guid? porfolioID {get;set;}
+        public decimal? CurrentInvestment { get; set; } //These three values will be used case by case - Just depending on which amount needs to be udated
+        public decimal? Liquid { get; set; }// Each one is optional so we wont get a callback if its empty
+        public decimal? CurrentTotal { get; set; }
+    }//End of Portfolio from front end to update portfolio in DB
+
+    /// <summary>
+    /// This Buy DTO has everything back end will need to create a Buy table for a specific portfolio
+    /// </summary>
+    public class Buy
+    {
+        public Guid? Fk_PortfolioID { get; set; }
+        public string? Symbol { get; set; }
+        public decimal? CurrentPrice { get; set; }
+        public decimal? AmountBought { get; set; }
+        public decimal? PriceBought { get; set; }
+        public Buy(){}
+        public Buy(Guid? Fk_PortfolioID, string? Symbol, decimal? CurrentPrice, decimal? AmountBought, decimal? PriceBought)
+        {
+            this.Fk_PortfolioID = Fk_PortfolioID;
+            this.Symbol = Symbol;
+            this.CurrentPrice = CurrentPrice;
+            this.AmountBought  = AmountBought;
+            this.PriceBought = PriceBought;
+        }
+    }//End of BUY
+
+
+    /// <summary>
+    /// This SELL DTO has everything back end will need to create a SELL table for a specific portfolio
+    /// </summary>
+    public class Sell
+    {
+        public Guid? Fk_PortfolioID { get; set; }
+        public string? Symbol { get; set; }
+        public decimal? AmountSold { get; set; }
+        public decimal? PriceSold { get; set; }
+        public decimal? PNL { get; set; }
+        public Sell(Guid? Fk_PortfolioID, string? Symbol, decimal? AmountSold, decimal? PriceSold, decimal? PNL)
+        {
+            this.Fk_PortfolioID = Fk_PortfolioID;
+            this.Symbol = Symbol;
+            this.AmountSold = AmountSold;
+            this.PriceSold  = PriceSold;
+            this.PNL = PNL;
+        }
+    }//End of SELL
+    
+}
