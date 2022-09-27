0d42b0b4a4ebac04dc3b8bf8ae42815226d93d6f
Mikael T Jaworski <nameLess@mikaels-mbp.myfiosgateway.com>
2022-09-27 16:02:33 -0400

    Fixed HTTP and Auth0 NullInjectErrors

--- client/src/app/components/nav-bar/nav-bar.component.spec.ts
+++ client/src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -3,9 +3,11 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { Location, CommonModule} from '@angular/common';
 import { Router } from '@angular/router';
+import { AuthModule } from '@auth0/auth0-angular';
+import { baseURL } from 'src/app/Services/base-url';
 
-import { NavBarComponent } from './nav-bar.component';
 import { inject } from '@angular/core';
+import { NavBarComponent } from './nav-bar.component';
 
 describe('NavBarComponent', () => {
   let component: NavBarComponent;
@@ -13,7 +15,20 @@ describe('NavBarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule,
+        AuthModule.forRoot({
+          domain: 'dev-pxtkabk5.us.auth0.com',
+          clientId: 'XpigNZhlmh9GXncdhIqEy26BhT0M18yI',
+          httpInterceptor: {
+            allowedList: [
+              baseURL + '/CreateProfileAsync',
+              baseURL + '/GetProfileByUserIDAsync',
+              baseURL + '/EditProfileAsync'
+             ], //for now
+          }
+    
+        }),
+      ],
       declarations: [ NavBarComponent ]
     })
     .compileComponents();
