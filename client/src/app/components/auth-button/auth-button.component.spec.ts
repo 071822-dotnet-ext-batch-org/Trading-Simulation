67a0973906f5f19ddd5352d92c6625cc36e691c3
Mikael T Jaworski <nameLess@mikaels-mbp.myfiosgateway.com>
2022-09-27 15:38:36 -0400

    added nav bar routelink testing

--- client/src/app/components/auth-button/auth-button.component.spec.ts
+++ client/src/app/components/auth-button/auth-button.component.spec.ts
@@ -1,13 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Auth0ClientService } from '@auth0/auth0-angular/';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { Location, CommonModule} from '@angular/common';
+import { Router } from '@angular/router';
 
 import { AuthButtonComponent } from './auth-button.component';
+import { inject } from '@angular/core';
 
 describe('AuthButtonComponent', () => {
   let component: AuthButtonComponent;
   let fixture: ComponentFixture<AuthButtonComponent>;
 
   beforeEach(async () => {
+    
     await TestBed.configureTestingModule({
       declarations: [ AuthButtonComponent ]
     })
