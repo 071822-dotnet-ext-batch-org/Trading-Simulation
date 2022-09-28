b3736a642e122e91edb595cebf732df743639422
Chrisjon Perry <chrisjonperry@Chrisjons-MacBook-Pro.local>
2022-09-28 14:50:12 -0400

    Adding angular unit testing for users component

--- client/src/app/components/user/user.component.spec.ts
+++ client/src/app/components/user/user.component.spec.ts
@@ -38,7 +38,35 @@ describe('UserComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  
+    it('test header1 to be false', () => {
+      const data = fixture.nativeElement.querySelector('h1');
+      expect(data).toBeFalsy();
   });
-});
+   it('test div',()=>{
+    const data = fixture.nativeElement.querySelector('.row');
+    expect(data).toBeFalsy();
+   });
+   it('test header2',()=>{
+    const data = fixture.nativeElement.querySelector('h2');
+    expect(data).toBeFalsy();
+   });
+   it('test header5',()=>{
+    const data = fixture.nativeElement.querySelector('h5');
+    expect(data).toBeFalsy();
+   });
+   it('should create userpage', () =>{
+    const fixture =TestBed.createComponent(UserComponent)
+    const user = fixture.debugElement.componentInstance;
+    expect(user).toBeTruthy();
+   });
+   it('should create header1',()=>{
+    const fixture = TestBed.createComponent(UserComponent);
+    fixture.detectChanges();
+    const complied = fixture.debugElement.nativeElement;
+    expect(complied.querySelector('h1').textContent).toContain('Yoink,');
+   });
+   it('testing profileJSON',()=>{
+    expect(component.profileJson).toBe("")
+   });
+})
