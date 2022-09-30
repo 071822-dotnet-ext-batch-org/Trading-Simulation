f1c8328a6aed087d77be8ec916a325181b049ee9
Ian Carmichael <ian027@revature.net>
2022-09-30 17:00:27 -0400

    finished first sequence of documentation in the API

--- YoinkAPI/BusinessLayer/IYoinkBusinessLayer.cs
+++ YoinkAPI/BusinessLayer/IYoinkBusinessLayer.cs
@@ -94,7 +94,6 @@ namespace BusinessLayer
         /// <returns>A list of Investment objects populated with data from investmentDto named investment.</returns>
         Task<List<Investment?>> GetAllInvestmentsByPortfolioIDAsync(Guid? portfolioID);
 
-
         //Profile Section
 
 
@@ -120,21 +119,92 @@ namespace BusinessLayer
         //Homepage
 
 
+        /// <summary>
+        /// Retrieves a count of users in the database.
+        /// </summary>
+        /// <returns>Integer count of users.</returns>
         Task<int> GetNumberOfUsersAsync();
+
+        /// <summary>
+        /// Retrieves a count of all posts in the database.
+        /// </summary>
+        /// <returns>Integer count of posts.</returns>
         Task<int> GetNumberOfPostsAsync();
+
+        /// <summary>
+        /// Retrieves a count of all buys in the database.
+        /// </summary>
+        /// <returns>Integer count of buys.</returns>
         Task<int> GetNumberOfBuysAsync();
+
+        /// <summary>
+        /// Retrieves a count of all sells in the database.
+        /// </summary>
+        /// <returns>Integer count of sells.</returns>
         Task<int> GetNumberOfSellsAsync();
 
         //Posts
 
 
+        /// <summary>
+        /// Allows the user to create a new post in the database.
+        /// Requires logged in user via Auth0.  
+        /// </summary>
+        /// <param name="post">CreatePostDto</param>
+        /// <returns>new Post object named createdPost.</returns>
         Task<Post?> CreatePostAsync(string auth0Id, CreatePostDto post);
+
+        /// <summary>
+        /// Retrieves the full post feed from the database, ordered by time descending.
+        /// </summary>
+        /// <returns>List of post objects, named returnedPosts.</returns>
         Task<List<PostWithCommentCountDto>> GetAllPostAsync();
+
+        /// <summary>
+        /// Allows user to update a created post in the database.
+        /// Requires logged in user via Auth0.  
+        /// </summary>
+        /// <param name="editPostDto">EditPostDto</param>
+        /// <returns>Updated post named editedPost</returns>
         Task<Post?> UpdatePostAsync(string? auth0UserId, EditPostDto editPostDto);
+
+        /// <summary>
+        /// Allows the user to delete their own post.
+        /// Requires logged in user via Auth0.  
+        /// </summary>
+        /// <param name="postId">nullable Guid</param>
+        /// <returns>Confirmation of deletion.</returns>
         Task<Guid?> DeletePostAsync(string? auth0UserId, Guid? postId);
+
+        /// <summary>
+        /// Allows user to retrieve all Posts from a particular user, along with comment count.
+        /// Requires logged in user via Auth0.  
+        /// </summary>
+        /// <param name="userId">string</param>
+        /// <returns>A list of PostWithCommentCountDto containing the returned posts along with a count of comments. This is required because the database table named Posts did not include a count (int) of Comments.</returns>
         Task<List<PostWithCommentCountDto>> GetAllPostByUserIdAsync(string userId);
+
+        /// <summary>
+        /// Allows user to click on a post they want to see details about, and retrieve the applicable details, e.g., comments, etc.
+        /// Requires logged in user via Auth0.  
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns>A PostWithCommentCountDto object, named returnedPost, containing the returned posts along with a count of comments. This is required because the database table named Posts did not include a count (int) of Comments.</returns>
         Task<PostWithCommentCountDto?> GetPostByPostIdAsync(Guid? postId);
+
+        /// <summary>
+        /// Allows the user to like a post.
+        /// Requires logged in user via Auth0.  
+        /// </summary>
+        /// <param name="like"></param>
+        /// <returns>likeCount integer, (and triggers a +1 like to the Post on the Posts table in the database.)</returns>
         Task<int?> CreateLikeOnPostAsync(LikeDto like, string? auth0UserId);
+
+        /// <summary>
+        /// Deletes the user's like(s) on the selected post.
+        /// </summary>
+        /// <param name="unlike">LikeDto</param>
+        /// <returns>updated likeCount integer, (and triggers a -1 like to the Post on the Posts table in the database.)</returns>
         Task<int?> DeleteLikeOnPostAsync(LikeDto unlike, string? auth0UserId);
     }
 }
