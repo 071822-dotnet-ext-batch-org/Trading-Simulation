fd95c22d5a7ccda75e656d462dbb451c72e16e30
(Sam) Sosorbaram Bayaraa <sosorbaram048@revature.net>
2022-10-01 14:12:42 -0700

    created reply box, when user clicks on comment icon, it will open a reply box, with a send button

--- client/src/app/components/post-card/post-card.component.ts
+++ client/src/app/components/post-card/post-card.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from 'src/app/Models/Posts';
 import { GetProfileByUserIDService } from 'src/app/Services/get-profile-by-user-id/get-profile-by-user-id.service';
-
+import { MatDialog } from '@angular/material/dialog'; //coment box (sam)
+import { CommentsComponent } from '../comment/comments.component';
 @Component({
   selector: 'app-post-card',
   templateUrl: './post-card.component.html',
@@ -14,11 +15,17 @@ export class PostCardComponent implements OnInit {
   profile: any;
 
   constructor(   
-    private PSS: GetProfileByUserIDService
+    private PSS: GetProfileByUserIDService,
+    private dialog: MatDialog //Comment box from materials (sam)
   ) { }
 
   ngOnInit(): void {
     this.getProfile(this.post?.fk_UserID)
+  }
+
+  //When user clicks on comment bubble icon, it will open comment box (sam)
+  onReplyClick(){
+    this.dialog.open(CommentsComponent)
   }
 
   getProfile(userID: string): void {
