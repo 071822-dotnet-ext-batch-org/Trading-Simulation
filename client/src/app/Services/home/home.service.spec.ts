1202dadabf481a8287ca5962b8ff9fc62c60d5c0
Mikael T Jaworski <nameLess@mikaels-mbp.myfiosgateway.com>
2022-10-03 12:02:57 -0400

    dependency fixes

--- client/src/app/Services/home/home.service.spec.ts
+++ client/src/app/Services/home/home.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { HomeService } from './home.service';
@@ -6,7 +7,9 @@ describe('HomeService', () => {
   let service: HomeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(HomeService);
   });
 
